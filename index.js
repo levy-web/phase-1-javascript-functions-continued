b93d28ffa333fb5d55d7ef7de12e8ffcf1ddf817
levy <lnjiru43@gmail.com>
2022-11-29 00:26:28 +0300

    changed wrap adjective function to return an anonymous function

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // code your solution here
 function saturdayFun(activity="roller-skate") {
 
-    return `This Saturday, I want to ${activity}!`;
+  return `This Saturday, I want to ${activity}!`;
+
+}
+
+let mondayWork = function(activity="go to the office") {
+
+  return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(style="*") {
+
+  return (adjective="special") => {
+
+  return `You are ${style}${adjective}${style}!`;
 
   }
-
-  let mondayWork = function(activity="go to the office") {
-
-    return `This Monday, I will ${activity}.`;
-  }
-
-  let wrapAdjective = function(style="*") {
-
-    return function(adjective="special") {
-
-      return `You are ${style}${adjective}${style}!`;
-
-    }
 
 }
 
